5084d6630a28297ea427fbe91aa5682fdcfa986c
Kevin Welton <kevinwe@unity3d.com>
2024-06-27 08:53:02 -0700

    Get rid of extra helloworld program

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -1,28 +1,27 @@
-; Will clear the screen and print "Hello, world!" in different colors.
+;
+; Example
+;
 
 *=$0801
 !byte $0c,$08,$b5,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
 jmp main
 
-hellostr
-    !scr "hello, world!" ; Declare our string and null terminate it
-    !byte 0
+.hellotext
+    !scr "hello, world!",0
+    !set ofs = 14
 
 main
-    ; Clear the screen using a KERNAL (yes, with an "a") routine
-    lda #$93
-    jsr $ffd2
+    ldy #0           ; Initial string index is the first character
+    ldx #1           ; Start with color 1 (white)
 
-    ldx #0             ; Use X to index the string and screen memory
-
-printstring
-    lda hellostr, x    ; Load the most significant digit (values were written in reverse order)
-    beq done           ; If X was 0 this iteration, we're done.
-    sta $400, x        ; Write the value to screen memory (PAL)
-    txa                ; Transfer X to A so we get the zero bit set if we're done
-    sta $d800, x       ; Make the text different colors
-    inx
-    jmp printstring
-
-done
-    rts
+print
+    lda .hellotext,y
+    beq +           ; Null character? we are done.
+    sta $400+ofs,y  ; Write character to screen memory
+    txa             ; Copy x register to the accumulator
+    sta $d800+ofs,y
+    iny             ; Increment y index register for next character
+    inx             ; Increment x index register for next color
+    jmp print       ; Loop to print the next character until we've printed them all.
++
+    rts             ; Done
